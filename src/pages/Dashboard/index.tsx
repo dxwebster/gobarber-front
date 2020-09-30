ff0345477548ab5c3ab4cc3287c9aefe9bd819cf
dxwebster <adrianalimafm@gmail.com>
2020-09-29 21:27:27 -0300

    Appointments List on Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+  Section,
+  Appointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setselectedDate] = useState(new Date()); //  troca a lista de appointments quando selecionar a data
   const { signOut, user } = useAuth(); // função de autenticação
 
   return (
@@ -28,6 +40,65 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src={logoImg} alt="GoBarber" />
+              <strong>Adriana Lima</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock /> 08:00
+              </span>
+              <div>
+                <img src={logoImg} alt="GoBarber" />
+                <strong> Adriana Lima</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock /> 08:00
+              </span>
+              <div>
+                <img src={logoImg} alt="GoBarber" />
+                <strong> Adriana Lima</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock /> 08:00
+              </span>
+              <div>
+                <img src={logoImg} alt="GoBarber" />
+                <strong> Adriana Lima</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
