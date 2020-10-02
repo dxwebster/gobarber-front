abe51e8e5375bd18005bc9d6d8fd7f634f90cc7f
dxwebster <adrianalimafm@gmail.com>
2020-10-01 22:03:13 -0300

    Updates

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -89,7 +89,7 @@ const Profile: React.FC = () => {
         });
       }
     },
-    [addToast, history]
+    [addToast, history, updateUser]
   );
 
   const handleAvatarChange = useCallback(
@@ -111,7 +111,7 @@ const Profile: React.FC = () => {
         console.log(e.target.files[0]);
       }
     },
-    [addToast]
+    [addToast, updateUser]
   );
 
   return (
